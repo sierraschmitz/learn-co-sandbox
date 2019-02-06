a8b760257eacf59311f5017e4670e7b9b3068cb0
active-array-4094 <sierra.schmitz@wework.com>
2019-02-06 01:15:04 +0000

    Automatically backed up by Learn

--- week-9/index.js
+++ week-9/index.js
@@ -1,6 +1,8 @@
 // asynchronous programming
 // not asynchronous
 // synchronous == one at a time
+//asynchronous == not one at a time/all at once
 
+//we don't want to block the UI from loading until everything is done
 
 const data = fetch('http://api.github.com/users/mirandamon')
